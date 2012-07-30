3ca98f386b1d9bb3aba5885d66df104a0fd68231
Pawel Peter Flis <pawel_flis@silversoft.pl>
2012-07-30 16:25:21 +0200

    create records in kha.hrl

---
+++ include/kha.hrl
@@ -0,0 +1,31 @@
+-type project_id() :: integer().
+-type time() :: {int(), int(), int()}.
+-type tag() :: string().
+-type command() :: string().
+
+-record(notification,
+        {type          :: atom(), %% kha_notification_[type].erl
+         params        :: proplists:proplist()
+        }).
+
+-record(project,
+        {id            :: project_id(),
+         name          :: string(),
+         local         :: filename:filename(), %% git clone remote local
+         remote        :: string(),
+         build         :: list(command()),
+         notifications :: list(#notification())
+        }).
+
+-record(build,
+        {project       :: project_id(),
+         title         :: string() | 'undefined',
+         branch        :: string(),
+         revision      :: string(),
+         author        :: string(),
+         start         :: time(),
+         stop          :: time(),
+         exit          :: integer(),
+         output        :: list(string()),
+         tags          :: list(tag())
+        }).
