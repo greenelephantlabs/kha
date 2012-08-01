2bb1518c68abab406ba33a07b873af423c75104d
Gleb Peregud <gleber.p@gmail.com>
2012-08-01 17:04:42 +0200

    implement project-local hooks

--- src/kha_hooks.erl
+++ src/kha_hooks.erl
@@ -13,27 +13,30 @@
 -export([run/3]).
 
 -define(HOOKS_LOCATION, <<"support/hooks/">>).
--define(HOOK(Name), (fun() ->
-                            case Name of
-                                on_success -> <<"on_success">>;
-                                on_failed  -> <<"on_failed">>;
-                                on_building   -> <<"on_building">>;
-                                on_timeout -> <<"on_timeout">>;
-                                _ -> throw({error, undefined_hook})
-                            end
-                    end)()).
+
+hookfn(on_success) -> <<"on_success">>;
+hookfn(on_failed)  -> <<"on_failed">>;
+hookfn(on_building)   -> <<"on_building">>;
+hookfn(on_timeout) -> <<"on_timeout">>.
 
 run(Which, ProjectId, BuildId) ->
+    {ok, P} = kha_project:get(ProjectId),
+    {ok, B} = kha_build:get(ProjectId, BuildId),
+    %%run global hook
     SelfPath = kha_utils:get_app_path(),
-    HookPath = filename:join([SelfPath, ?HOOKS_LOCATION ,?HOOK(Which)]),
+    HookPath = filename:join([SelfPath, ?HOOKS_LOCATION, hookfn(Which)]),
+    do_run(Which, HookPath, P, B),
 
+    %%run project hook
+    Local = kha_utils:convert(P#project.local, str),
+    LocalHookPath = filename:join([Local, ?HOOKS_LOCATION, hookfn(Which)]),
+    do_run(Which, LocalHookPath, P, B).    
+
+do_run(Which, HookPath, P, B) ->
     case filelib:is_file(HookPath) of
         true ->
-            {ok, P} = kha_project:get(ProjectId),
-            {ok, B} = kha_build:get(ProjectId, BuildId),
-
             Opts = io_lib:fwrite("~b ~b \"~s\" \"~s\" \"~s\" \"~s\"",
-                                 [ProjectId, BuildId, B#build.title,
+                                 [P#project.id, B#build.id, B#build.title,
                                   B#build.branch, B#build.author,
                                   P#project.remote]),
             Cmd = io_lib:fwrite("~s ~s", [HookPath, Opts]),
@@ -46,5 +49,5 @@ run(Which, ProjectId, BuildId) ->
             end;
         false ->
             ?LOG("Hook for ~p not exist (not found ~s file)",
-                 [Which, ?HOOK(Which)])
+                 [Which, HookPath])
     end.
