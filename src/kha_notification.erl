37f9f34be6e5debbab5db5be0d26d8943e6932ac
Paul Peter Flis <pawel_flis@silversoft.pl>
2012-09-13 10:53:42 +0200

    more info in emails notifications

--- src/kha_notification.erl
+++ src/kha_notification.erl
@@ -8,14 +8,28 @@
 run(Project, Build) ->
     Notif = Project#project.notifications,
     PId = Project#project.id,
+    TitleProject = Project#project.name,
     BId = Build#build.id,
     Status = Build#build.status,
     Output = Build#build.output,
+
+    More = io_lib:format("Start: ~s~n"
+                         "Stop: ~s~n"
+                         "Author: ~s~n"
+                         "Branch: ~s~n"
+                         "Revision: ~s~n"
+                         "Exit code: ~b~n~n",
+                         [kha_utils:now_to_nice(Build#build.start),
+                          kha_utils:now_to_nice(Build#build.stop),
+                          Build#build.author, Build#build.branch,
+                          Build#build.revision, Build#build.exit]),
     
-    Title = io_lib:format("Project: ~b; Build: ~b - ~p", [PId, BId, Status]),
+    Title = io_lib:format("[~p] Project: ~b (~s); Build: ~b",
+                          [Status, PId, TitleProject, BId]),
     [ begin
           ModuleName = type_to_module(Type),
-          do_run(ModuleName, Title, Output, Params)
+          Content = [ More | lists:reverse(Output)],
+          do_run(ModuleName, Title, Content, Params)
       end || #notification{type = Type, params = Params} <- Notif ].
 
 do_run(ModuleName, Title, Content, Params) ->
