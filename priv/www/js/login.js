44f9581e6cf03b84cd9f4fc6e3aa089da74bac7b
Gleb Peregud <gleber.p@gmail.com>
2012-10-01 18:45:14 +0200

    implement logout in UI

--- priv/www/js/login.js
+++ priv/www/js/login.js
@@ -1,11 +1,25 @@
 angular.module('Kha').controller({
   LoginController: function ($scope, $http, authService) {
-    $scope.submit = function() {
-      $http.post('user/login', {email: $scope.email, password: $scope.password}).success(function() {
+    $scope.login = function() {
+      $http.post('user/login', {email: $scope.email, password: $scope.password}).success(function(data) {
+        if (data.result === true) {
+          $scope.session = data.session;
+        }
         authService.loginConfirmed();
       }).error(function() {
+        $scope.session = null;
         alert('Login fail!');
       });
-    }
+    };
+    $scope.logout = function() {
+      $http.post('/user/logout', {}).success(function() {
+        $scope.session = null;
+      });
+    };
+    $http.get('/user/session').success(function(data) {
+      if (data.result === true) {
+        $scope.session = data.session;
+      };
+    });
   }
 });
