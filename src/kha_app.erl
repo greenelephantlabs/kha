089d2f63aee00e162d55feac29908afc4e93bc5d
Gleb Peregud <gleber.p@gmail.com>
2012-10-01 18:44:39 +0200

    don't forget to start crypto and bcrypt

--- src/kha_app.erl
+++ src/kha_app.erl
@@ -13,6 +13,8 @@
 
 start() ->
     db:init(),
+    ok = application:start(crypto, permanent),
+    ok = application:start(bcrypt, permanent),
     ok = application:start(ranch, permanent),
     ok = application:start(cowboy, permanent),
     ok = application:start(mimetypes, permanent),
