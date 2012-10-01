d7394d5b3a1de2e423e15ac0a191a68f8d4c1081
Gleb Peregud <gleber.p@gmail.com>
2012-10-01 19:35:54 +0200

    notify user about need to login

--- priv/www/js/login.js
+++ priv/www/js/login.js
@@ -1,5 +1,6 @@
 angular.module('Kha').controller({
   LoginController: function ($scope, $http, authService) {
+    $scope.urgent = false;
     $scope.login = function() {
       $http.post('user/login', {email: $scope.email, password: $scope.password}).success(function(data) {
         if (data.result === true) {
@@ -20,6 +21,12 @@ angular.module('Kha').controller({
       if (data.result === true) {
         $scope.session = data.session;
       };
+    });
+    $scope.$on('event:auth-loginRequired', function() {
+      $scope.urgent = true; //GP: it does not handle the case when user is logged in, but does not have rights to perform an action
+    });
+    $scope.$on('event:auth-loginConfirmed', function() {
+      $scope.urgent = false;
     });
   }
 });
