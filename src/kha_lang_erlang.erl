775f4227c2f47f12d9dc98989fb8591673779a98
Gleb Peregud <gleber.p@gmail.com>
2013-01-06 16:54:09 +0100

    handle both cases of kerl installations

--- src/kha_lang_erlang.erl
+++ src/kha_lang_erlang.erl
@@ -5,5 +5,5 @@
 set_runtime_cmd(default) ->
     [];
 set_runtime_cmd(Ver0) ->
-    Ver = iolist_to_binary(Ver0),
-    [<<". `kerl list installations | egrep '^", Ver/binary, " ' | awk '{print $2}'`/activate">>].
+    Ver = iolist_to_binary(string:to_upper(kha_utils:convert(Ver0, str))),
+    [<<". `kerl list installations | egrep -i '^", Ver/binary, " ' | awk '{print $2}'`/activate">>].
